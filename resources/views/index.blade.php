a85f90c17400b4c6a7a2f75793a53dd8be2d1de5
YeYintKo <yeyintko@ucs-myitkyina.edu.mm>
2020-11-23 20:06:34 +0630

    complete admin dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+
+
+@section('content')
+
+{{-- <livewire:counter /> --}}
+
+@livewire('post-component',['title'=>'Title 1','detail'=>'ected an abnormally high volume of traffic on our site. To let us know you’re a human and not a BOT, please pass the above security test. We’ll remember your response and won’t show you this security check again.','author'=>"YeYintKo"])
+@livewire('post-component',['title'=>'Title 2','detail'=>'ected an abnormally high volume of traffic on our site. To let us know you’re a human and not a BOT, please pass the above security test. We’ll remember your response and won’t show you this security check again.','author'=>"YeYintKo"])
+@livewire('post-component',['title'=>'Title 3','detail'=>'ected an abnormally high volume of traffic on our site. To let us know you’re a human and not a BOT, please pass the above security test. We’ll remember your response and won’t show you this security check again.','author'=>"YeYintKo"])
+@livewire('post-component',['title'=>'Title 4','detail'=>'ected an abnormally high volume of traffic on our site. To let us know you’re a human and not a BOT, please pass the above security test. We’ll remember your response and won’t show you this security check again.','author'=>"YeYintKo"])
+
+
+@endsection
